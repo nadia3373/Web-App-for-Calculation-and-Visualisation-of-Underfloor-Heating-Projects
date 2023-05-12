6cf3c21dc67ff809e47e84265610204e8a220ad1
nadia3373 <3773373@mail.ru>
2023-05-12 11:06:50 +0700

    changed Angular port to 80

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,8 +6,8 @@ from api.rooms import router as rooms_router
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:8080",
-    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://localhost",
 ]
 
 app.add_middleware(
